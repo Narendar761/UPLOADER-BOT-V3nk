53ec3c067236e2eb00babbbc42d0eaa66fbdc7df
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2023-10-31 10:55:22 +0530

    Update add.py

--- plugins/database/add.py
+++ plugins/database/add.py
@@ -4,6 +4,12 @@ from plugins.config import Config
 from .database import db
 from pyrogram import Client
 from pyrogram.types import Message
+from pymongo import MongoClient
+
+# Replace 'localhost' with the hostname or IP address of your MongoDB server.
+# Replace 27017 with the port number of your MongoDB server if it's not the default.
+client = MongoClient('localhost', 27017)
+
 
 
 async def add_user_to_database(bot: Client, cmd: Message):
